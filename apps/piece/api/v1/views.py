caddd37d16d9fff422611cfc6466e1304f7982bd
ravellys <ravellyspyrrho@gmail.com>
2022-01-04 22:44:43 -0300

    Method to receive the piece type/name and the color and return the piece id;

--- apps/piece/api/v1/views.py
+++ apps/piece/api/v1/views.py
@@ -1,8 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from apps import piece
 from apps.piece.api.serializers import PieceColorSerializer, PieceSerializer
 from apps.piece.models import PieceColor, Piece
 
@@ -21,10 +20,39 @@ class PieceViewSet(viewsets.ModelViewSet):
     """
     queryset = Piece.objects.all()
     serializer_class = PieceSerializer
+    alias_params = {
+        "color__abbreviation": ['color', 'color_abreviation', 'c'],
+        "notation": ['notation', 'name', 'type']
+    }
+
+    def has_parameters(self, params: dict) -> bool:
+        mandatory_parameters = self.alias_params.keys()
+        if mandatory_parameters - set(params.keys()):
+            return False
+        return True
+
+    def convert_params(self, params: dict) -> dict:
+        params_converted = {}
+        for key, value in params.items():
+            for name, alias_list in self.alias_params.items():
+                if key in alias_list:
+                    params_converted[name] = value
+        return params_converted
 
     @action(detail=False, methods=['get'])
     def get_piece_id(self, request):
-        piece = Piece.objects.filter()
-        serializer = self.serializer_class(piece, many=True)
-        return Response(serializer.data)
+        params = request.GET.dict()
+        params = self.convert_params(params)
 
+        if not self.has_parameters(params):
+            return Response(
+                {"message": f"Missing Parameters. Please insert the mandatory parameters: " +
+                            f"({' and '.join([ ' or '.join(alias) for alias in list(self.alias_params.values())])})"},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        piece = Piece.objects.filter(**params).first()
+
+        if not piece:
+            return Response({"message": "Piece Not Founded"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"id": piece.id})
